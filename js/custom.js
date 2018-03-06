c1be02eebb30384ab022452ef81fed8a9446c82e
ihardy <ihardy@wrlc.org>
2018-03-06 10:04:22 -0500

    specify central

--- js/custom.js
+++ js/custom.js
@@ -2,18 +2,10 @@
     "use strict";
     'use strict';
 
+    
+    var app = angular.module('centralCustom', ['angularLoad'];)
 
-    var app = angular.module('viewCustom', ['angularLoad']);
-
-    /****************************************************************************************************/
-
-        /*In case of CENTRAL_PACKAGE - comment out the below line to replace the other module definition*/
-
-        /*var app = angular.module('centralCustom', ['angularLoad']);*/
-
-    /****************************************************************************************************/
-
-
+    
 })
 
    

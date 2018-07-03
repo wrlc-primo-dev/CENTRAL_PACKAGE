166d61545643ab6a20e3ce3b052c5b6bc15a44a9
ihardy <ihardy@wrlc.org>
2018-07-03 14:15:47 -0400

    use a different directive to solve conflict w gw

--- js/custom.js
+++ js/custom.js
@@ -4,7 +4,7 @@
 
     var app = angular.module('centralCustom', ['angularLoad']);
 
-    app.component('prmExploreMainAfter', {
+    app.component('prmSilentLoginAfter', {
     template: `
 	<div id="wrlc-footer" layout="row" layout-align="center center">
 	    <div id="wrlc-footer-message" layout-align="center center">Copyright 2018 WRLC. This website is a service of the <a href="https://www.wrlc.org">Washington Research Library Consortium</a></div>

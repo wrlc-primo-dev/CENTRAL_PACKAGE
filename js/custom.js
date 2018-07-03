48ce0657a8e7adfa6663b39d9f92a3057b712688
ihardy <ihardy@wrlc.org>
2018-07-03 10:40:22 -0400

    adds wrlc custom footer

--- js/custom.js
+++ js/custom.js
@@ -2,10 +2,15 @@
     "use strict";
     'use strict';
 
-    
-    var app = angular.module('centralCustom', ['angularLoad'];)
+    var app = angular.module('centralCustom', ['angularLoad']);
 
-    
-})
+    app.component('prmExploreMainAfter', {
+    template: `
+	<div id="wrlc-footer" layout="row" layout-align="center center">
+	    <div id="wrlc-footer-message" layout-align="center center">Copyright 2018 WRLC. This website is a service of the <a href="https://www.wrlc.org">Washington Research Library Consortium</a></div>
+	</div>
+    `
 
-   
+    });
+
+})();
